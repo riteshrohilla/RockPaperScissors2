bd5a4eb54c3ba6610e769b734569f57d50a31748
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-21 14:15:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choices = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let winningConditions = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper',
+    }
+
 }
