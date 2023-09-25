dc6083ffc33a7939cca46c8545d6620c02b00570
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-25 22:31:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors']
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
