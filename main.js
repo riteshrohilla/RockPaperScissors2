dda1a92964d0bae6318a112c9ac7fd0235773396
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-09-26 00:04:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection === playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!"
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        return 'You win ' + playerSelection + ' beats ' + computerSelection;
+    } else {
+        return 'You lose ' + computerSelection + ' beats ' + playerSelection;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
